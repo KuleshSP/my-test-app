d47feee2b01b9e24b251af71dc9250ddaef0ac71
Sergey <kulesh.sp@gmail.com>
2022-12-17 02:01:20 +0400

    Resolves build issue

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,15 @@ export default defineConfig({
   esbuild: {
     logOverride: {'this-is-undefined-in-esm': 'silent'},
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
